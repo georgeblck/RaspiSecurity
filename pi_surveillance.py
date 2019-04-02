5c77d356c139197710c5ccb7a51ca3701749cfa0
Nikolai Huckle <n.huckle@posteo.de>
2019-04-02 17:39:28 +0200

    troubleshooting the playing voice

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -163,8 +163,8 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
                 if conf["activate_button"]:
                     movie1 = ("/home/pi/Videos/[MEME] Why are you gay _.mp4")
                     movie2 = ("/home/pi/Videos/SPUNKY BE SNIFFIN ASS.mp4")
-                    say_weather("test")
-                    playVidwaitButton(movie1, movie2, int(conf["which_gpio"]))
+                    say_weather("testtesttesttesttest")
+                    # playVidwaitButton(movie1, movie2, int(conf["which_gpio"]))
                     print("Played video")
 
                 # update the last uploaded timestamp and reset the motion
